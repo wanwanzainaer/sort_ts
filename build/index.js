358add38466f67a068c95f326957ab27e0b14ee0
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-12 14:18:45 -0800

    Add characters collection class

--- build/index.js
+++ build/index.js
@@ -2,7 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([12, 3, -5, 0]);
+var stringCollection = new CharactersCollection_1.CharactersCollection('XssdAfc');
+var characterSorter = new Sorter_1.Sorter(stringCollection);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
+characterSorter.sort();
 console.log(numbersCollection.data);
+console.log(stringCollection.data);
+//comment
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 const numbersCollection = new NumbersCollection([12, 3, -5, 0]);
+const stringCollection = new CharactersCollection('XssdAfc');
+const characterSorter = new Sorter(stringCollection);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
+characterSorter.sort();
 console.log(numbersCollection.data);
+console.log(stringCollection.data);
 //comment
